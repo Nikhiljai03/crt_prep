2a02e9df2e952fa860d5d46620372c86bed6b8a6
NIHKIL JAI <nikhilraina95@gmail.com>
2025-09-11 20:41:43 +0530

    day 4

--- crt_11_09/pp.py
+++ crt_11_09/pp.py
@@ -1,4 +1,3 @@
-
 # Convert a number n from base g to base d
 
 # n = int(input())
@@ -21,6 +20,21 @@
 #     p1 += 1
 #     s //= d
 # print(s1)
+
+# n=123
+# ans=0
+# while(n>10):
+#     d=n%100
+#     ans+=d
+#     n//=10
+# print(ans)
+
+# a=1
+# while(n>0):
+#     r=n%10
+#     a*=r
+#     n//=10
+# print(a)
 
 # Convert a number to any base  
 # n = int(input())
@@ -63,3 +77,15 @@
 #             s //= d
 #         print(s1)
         
+# n = int(input())
+# sum = 0
+# while(n > 10):
+#     r = n % 100
+#     rev = 0
+#     while(r > 0):
+#         r1 = r % 10
+#         rev = rev * 10 + r1
+#         r //= 10
+#     sum += rev
+#     n = n // 10
+# print(sum)
